7c9fed9d88bfa40f491df10a1a349e9b5f9fb83e
Dayles1 <Sololeveling0025@gmail.com>
2025-05-28 10:41:42 +0500

    php artisan make:interface Repositories/AuthRepositoryInterface

--- app/Interfaces/Interfaces/Repositories/UserRepositoryInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Interfaces\Interfaces\Repositories;
-
-interface UserRepositoryInterface
-{
-    //
-}
--- app/Interfaces/Repositories/AuthRepositoryInterface.php
+++ app/Interfaces/Repositories/AuthRepositoryInterface.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Interfaces\Interfaces\Repositories;
+namespace App\Interfaces\Repositories;
 
 interface AuthRepositoryInterface
 {
