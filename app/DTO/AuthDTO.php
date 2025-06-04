86ca9e78b19a1c83c2016e5155144c4787870fb8
Dayles1 <Sololeveling0025@gmail.com>
2025-06-04 13:13:26 +0500

    +DTO logic

--- app/DTO/AuthDTO.php
+++ app/DTO/AuthDTO.php
@@ -2,10 +2,30 @@
 
 namespace App\DTO;
 
+use Illuminate\Http\UploadedFile;
+
 class AuthDTO
 {
-    
-    public function __construct()
+    public ?string $name;
+    public ?string $last_name;
+    public string $phone;
+    public ?UploadedFile $avatar;
+
+    public function __construct(string $phone, ?string $name = null, ?string $last_name = null, ?UploadedFile $avatar = null)
     {
+        $this->phone = $phone;
+        $this->name = $name;
+        $this->last_name = $last_name;
+        $this->avatar = $avatar;
+    }
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            $data['phone'], 
+            $data['name'] ?? null,
+            $data['last_name'] ?? null,
+            $data['avatar'] ?? null
+        );
     }
 }
