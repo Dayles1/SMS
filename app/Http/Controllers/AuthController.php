4f2727aac305b3842ef2567250273e8a1baa585c
Dayles1 <Sololeveling0025@gmail.com>
2025-06-04 11:50:59 +0500

    php artisan make:provider InterfaceProvider

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Interfaces\Services\AuthServiceInterface;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function __construct(protected AuthServiceInterface $authService)
+    {
+
+    }
+    public function register(){
+
+    }
 }
